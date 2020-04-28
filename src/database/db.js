bded789c78201125c66d43e1dc7a2538f7826d14
gelintonx <smartgelix@gmail.com>
2020-04-28 16:32:53 +0200

    third commit

--- src/database/db.js
+++ src/database/db.js
@@ -1,9 +1,9 @@
 const {Pool} = require('pg')
 const pool = new Pool({
-    user: process.env.DB_USER || "jwzqvdzyoyyeti",
-    host: process.env.DB_HOST || "ec2-54-75-244-161.eu-west-1.compute.amazonaws.com",
-    database: process.env.DATABASE || "d4kp8et36pmkfo",
-    password: process.env.DB_PASSWORD || "93260bd4c555e083a4ca55a37c6f5eac65fe4388f825cdf8149e07910465b4cd"
+    user: process.env.DB_USER || "jpzkwrcazcughn",
+    host: process.env.DB_HOST || "ec2-54-246-87-132.eu-west-1.compute.amazonaws.com",
+    database: process.env.DATABASE || "d541utomubjgau",
+    password: process.env.DB_PASSWORD || "ce6a8e39de2ffc09d3625853cb651f00ce6823ec97d850d90cead27b0ff2eb3b"
 })
 
 module.exports = pool;
