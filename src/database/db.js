91047084bc7fbb2f37cf6fc89db0f939efe6aba6
gelintonx <60986235+gelintonx@users.noreply.github.com>
2020-12-06 11:35:46 +0100

    Update db.js

--- src/database/db.js
+++ src/database/db.js
@@ -1,9 +1,9 @@
 const {Pool} = require('pg')
 const pool = new Pool({
-    user: process.env.DB_USER || "jpzkwrcazcughn",
-    host: process.env.DB_HOST || "ec2-54-246-87-132.eu-west-1.compute.amazonaws.com",
-    database: process.env.DATABASE || "d541utomubjgau",
-    password: process.env.DB_PASSWORD || "ce6a8e39de2ffc09d3625853cb651f00ce6823ec97d850d90cead27b0ff2eb3b"
+    user: process.env.DB_USER ,
+    host: process.env.DB_HOST ,
+    database: process.env.DATABASE,
+    password: process.env.DB_PASSWORD
 })
 
 module.exports = pool;
